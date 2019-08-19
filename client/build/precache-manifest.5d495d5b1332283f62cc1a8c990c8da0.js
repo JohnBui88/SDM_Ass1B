268737b1ae65998b01957c366819dc8f2c083f56
John Bui <john.bui88@gmail.com>
2019-08-19 16:52:01 +1200

    commit to test_branch

---
+++ client/build/precache-manifest.5d495d5b1332283f62cc1a8c990c8da0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e3fbf6c50cfeaff2d629c964e314b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc428d1f5419797daeec",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "571fcc740d9065ae0a0c",
+    "url": "/static/js/2.71410c93.chunk.js"
+  },
+  {
+    "revision": "dc428d1f5419797daeec",
+    "url": "/static/js/main.6da5b783.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ config/dev.js
@@ -0,0 +1,9 @@
+//dev.js - don't commit this
+module.exports = {
+  googleClientID:
+    "396907681615-fgf9m5c5qht6fdimik41olcee78pl3nk.apps.googleusercontent.com",
+  googleClientSecret: "NriKPTbcF1MMYfyQFKkxzGsF",
+  mongoURI:
+    "mongodb+srv://johnbui:1234Abcd@cluster0-qv0ln.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "fjadsifjfhdsakiwuoijk"
+};
